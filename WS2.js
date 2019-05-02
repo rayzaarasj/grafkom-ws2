0b00a43b971761dbb19be0dd762879265a1154ba
rayzaarasj <rayza.arasj@gmail.com>
2019-05-02 22:58:35 +0700

    merged conflict

--- WS2.js
+++ WS2.js
@@ -143,17 +143,10 @@ var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0);
 var lightDiffuse = vec4(1.0, 1.0, 1.0, 1.0);
 var lightSpecular = vec4(1.0, 1.0, 1.0, 1.0);
 
-<<<<<<< HEAD
-var materialAmbient = vec4(1.0, 0.0, 1.0, 1.0);
-var materialDiffuse = vec4(1.0, 0.8, 0.0, 1.0);
-var materialSpecular = vec4(1.0, 0.8, 0.0, 1.0);
-var materialShininess = 10.0;
-=======
 var materialAmbient = vec4(1.0, 1.0, 1.0, 1.0);
 var materialDiffuse = vec4(1.0, 1.0, 1.0, 1.0);
 var materialSpecular = vec4(1.0, 1.0, 1.0, 1.0);
 var materialShininess = 100.0;
->>>>>>> e8e66ea842c2f6382584bc65e66d4a50137bb768
 
 // Parameters for texture and image
 
